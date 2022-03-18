76903e5ca0153900ad2f83f256f3e5f20a32e079
AnnaTerelius <tereliusanna@hotmail.com>
2022-03-18 13:54:46 +0100

    added img

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import React, {useState, useEffect} from 'react';
 import logo from './starstable.png';
+import horses from './horses.png'
 import './App.css';
-//{tasks.filter(item => item.text.toLowerCase().includes(param.toLowerCase()))
-
-
-
-
 
 function App() {
   const [tasks, setTasks] = useState(null);
   const [text, setText] = useState('');
-  const [taskId, setTaskId] = useState(null);
   const [param, setParam] = useState('');
-  const [searchedTask, setSearchedTask] = useState('');
   const [filter, setFilter] = useState('All');
-
-  //const FILTER_NAMES = Object.keys(FILTER_MAP);
 
   const FILTER_MAP = {
     All: () => true,
@@ -24,17 +16,6 @@ function App() {
     Searchstring: task => task.text.toLowerCase().includes(param.toLowerCase()) 
   };
 
-  /*const filterList = FILTER_NAMES.map(name => (
-    <FilterButton
-      key={name}
-      name={name}
-      isPressed={name === filter}
-      setFilter={setFilter}
-    />
-  ));*/
- 
-  
-
   useEffect(() =>{
     fetch('/api/tasks')
     .then(res => res.json())
@@ -42,70 +23,37 @@ function App() {
     .catch(err => console.log(err))
   }, [])
 
-  const submitParamForm = async (event) => {
-    console.log("inside submitparamform")
-    event.preventDefault()
-    try {
-      const res = await fetch(`/api/task/${param}`)
-      const json = await res.json()
-      console.log(json)
-
-      setParam('')
-    }catch (err) {
-
-    }
-  }
-
   const submitForm = async (event) => {
     event.preventDefault()
     try {
       const res = await fetch('/api/tasks', { method: 'POST', body: JSON.stringify({"text": text, "done": false })})
       const json = await res.json()
-
       setTasks([...tasks, json.task])
       setText('')
-    
     }catch (err) {
-
     }
   }
 
   const deleteTask = async (id) => {
-    console.log("inside deleteTasks")
-    console.log(id)
     try {
      await fetch(`/api/tasks/${id}`, { method: 'DELETE' })
-      console.log(id)
-      setTasks(tasks.filter((task) => task.id !== id))
-      
+      setTasks(tasks.filter((task) => task.id !== id)) 
     }catch (err) {
-    console.log(err)
     }
-
   }
 
-  const upDateBolean = async (id) => {
-    console.log("inside upDateBolean...")
-    console.log(id)
+  const upDateBoolean = async (id) => {
     const task = tasks.find((task) => task.id === id)
     if(!task) return
     try {
      const res = await fetch(`/api/tasks/${id}`, { method: 'PATCH' })
-      console.log({id})
       const json = await res.json()
-      console.log(res)
-      console.log(json)
       const tasksCopy = [...tasks]
       const index = tasks.findIndex(task => task.id === id)
       tasksCopy[index] = json
-      console.log(tasksCopy)
       setTasks(tasksCopy)
-     
-      console.log("done with updating")
     }catch (err) {
-    console.log(err)
     }
-
   }
 
   return (
@@ -113,39 +61,35 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
           <div className="to-do-background">
-          <div className="form-container">
-             
+          <div className="form-container"> 
             {tasks?.length > 0 ? (
             <ul>
               <div className="form-header">
                 <div>
                   <h1>To do</h1>
-                  <div>
-                <form className="form" onSubmit={submitForm}>
-                  <div>
-                    <input type="text" placeholder="add new task" value={text} onChange={e => setText(e.target.value)}></input>
-                  </div>
-                  <div>
-                    <button type="submit">Add</button>
-                  </div>
-                </form>
-              </div>
-                </div>
-                <form className="paramForm" onSubmit={submitParamForm}>
-                  <div>
-                      <input type="text" placeholder="search by text" value={param} onChange={e => {setParam(e.target.value);setFilter("Searchstring")}}></input>
-                      {console.log(param)}
-                  </div>
                   <form className="checkboxes">
                     <input type="checkbox" id="allTodos" name="allTodos" value="all" onClick={() => setFilter("All")}></input>
-                    {console.log(filter)}
                     <label> Show all tasks</label><br></br>
                     <input type="checkbox" id="doneTodos" name="doneTodos" value="done" onClick={() => setFilter("Completed")}></input>
                     <label> Show done tasks</label><br></br>
                     <input type="checkbox" id="todo" name="todo" value="todo" onClick={() => setFilter("Active")}></input>
                     <label> Show tasks to do</label><br></br>
                   </form>
+                  <div>
+                    <input type="text" placeholder="search by text" value={param} onChange={e => {setParam(e.target.value);setFilter("Searchstring")}}></input>
+                  </div>
+                  <div>
+                <form className="form" onSubmit={submitForm}>
+                  <div>
+                    <input type="text" placeholder="add new task" value={text} onChange={e => setText(e.target.value)}></input>
+                  </div>
+                  <div>
+                    <button className="btn" type="submit">Add</button>
+                  </div>
                 </form>
+              </div>
+                </div>
+                <img src={horses} className="img-horses" alt="horses" />
               </div>
                 {tasks.filter(FILTER_MAP[filter]).map(({id, text, done}) =>(
                   <li key={id}>
@@ -155,7 +99,7 @@ function App() {
                     </div>
                     <div className="list-btn-container">
                       <button className="list-btn"onClick={() => deleteTask(id)}>&#10060;</button>
-                      <button className="list-btn" onClick={() => upDateBolean(id)}>&#9989;</button>
+                      <button className="list-btn" onClick={() => upDateBoolean(id)}>&#9989;</button>
                     </div>
                   </li>
                 ))}
--- src/server.js
+++ src/server.js
@@ -1,61 +1,38 @@
 import { createServer } from "miragejs"
 
 let tasks = [
-  { id: 1, text: "Buy ... wheat", done: false },
+  { id: 1, text: "Buy wheat", done: false },
   { id: 2, text: "Make bread", done: false },
 ]
 
 createServer({
   routes() {
     this.namespace = "api"
-
      this.post("/tasks", (schema, request) => {
       let attrs = JSON.parse(request.requestBody)
       attrs.id = Math.floor(Math.random() * 100)
       tasks.push(attrs)
-
       return { task: attrs }
     })
-
     this.get("/tasks", () => {
       return {
         tasks
       }
     })
-    
- 
+     
     this.get("/task/:search", (schema, request) => {
       try {
-        console.log("hejsan!")
-        console.log(request)
         const searchString = request.params.search;
-        console.log(searchString)
         let filteredTasks = tasks.filter(item => item.text.toLowerCase().includes(searchString.toLowerCase())) 
-        console.log(filteredTasks)
         return filteredTasks
       }catch (err) {
-        console.log(err)
         } 
     })
 
-    /*this.get("/task/:search", (req, res) => {
-      console.log("hejsan!")
-      const searchString = req.query.search;
-      let filteredTask = tasks.filter(item => {
-        const itemText = item.text.toString();
-        return itemText.toLowerCase().includes(filteredTask.toLowerCase())
-      });
-      res.json(itemText)
-    });*/
-
-
-    //PATCH = PUT?
     this.patch("/tasks/:id", (schema, request) => {
       try { 
         let newAttrs = JSON.parse(request.requestBody)
         let id = request.params.id
-        console.log(id)
-        console.log(tasks)
         let task = tasks.find(item=>item.id==parseInt(id));
         if (task.done === true) {
           task.done = false
@@ -64,18 +41,13 @@ createServer({
       }
         return task
       }catch (err) {
-        console.log(err)
         }  
     })
     
-
     this.delete("/tasks/:id", (schema, request) => {
       let id = request.params.id
-      console.log(id)
-      let index = tasks.findIndex(task => task.id === parseInt(id))
-      
-      return delete tasks[index];
-      
+      let index = tasks.findIndex(task => task.id === parseInt(id))     
+      return delete tasks[index];     
     })
   },
 })
